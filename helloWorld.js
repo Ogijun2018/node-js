cb11b016dce0f00ba68b429fc461357e13f268f7
Jun Ogino <ogijun.mk@hotmail.co.jp>
2019-05-14 10:29:57 +0900

    HelloWorldを書き換え、発表済み

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,7 @@
 let http = require('http');
 
+
 let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
     res.write('Hello World!');
     res.end();
 });
