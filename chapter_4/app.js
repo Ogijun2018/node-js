61b3ec52d6ce159c2caf01aa937f97645b1efbe7
Jun Ogino <ogijun.mk@hotmail.co.jp>
2019-06-18 10:34:17 +0900

    chapter_4発表済み

--- chapter_4/app.js
+++ chapter_4/app.js
@@ -1,27 +1,14 @@
-let express = require('express');
-let app = express();
+let app = require('express')();
 let ejs = require('ejs');
-let bodyParser = require('body-parser'); //①
 
-app.engine('ejs', ejs.renderFile);
-app.use(bodyParser.urlencoded({ //②
-    extended: true
-}));
+app.engine('ejs', ejs.renderFile); //①
 
 app.get('/', (req,res) => {
-    console.log('---GET Request---');
-    console.log('name は'+req.query.name); //③
-    console.log('age は'+req.query.age);
-    res.render('postDataEjs.ejs',{});
-});
-
-app.post('/', (req,res) => { //④
-    console.log('---POST Request---');
-    console.log('name は'+req.body.name); //⑤
-    console.log('age は'+req.body.age);
-    res.render('postDataEjs.ejs',{});
+    res.render('tempEjs.ejs',{ //②
+        contents: 'hoge'
+    });
 });
 
 let server = app.listen(1234, () => {
     console.log('create server.');
-})
+});
---
+++ chapter_4/app2.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let app = express();
+let ejs = require('ejs');
+let bodyParser = require('body-parser'); //①
+
+app.engine('ejs', ejs.renderFile);
+app.use(bodyParser.urlencoded({ //②
+    extended: true
+}));
+
+app.get('/', (req,res) => {
+    console.log('---GET Request---');
+    console.log('name は'+req.query.name); //③
+    console.log('age は'+req.query.age);
+    res.render('postDataEjs.ejs',{});
+});
+
+// app.post('/', (req,res) => { //④
+//     console.log('---POST Request---');
+//     console.log('name は'+req.body.name); //⑤
+//     console.log('age は'+req.body.age);
+//     res.render('postDataEjs.ejs',{});
+// });
+
+let server = app.listen(1234, () => {
+    console.log('create server.');
+})
